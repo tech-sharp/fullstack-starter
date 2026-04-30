d7fe88b36531e1dad562c8a43d8c114e8fc733c5
tech-sharp <cool.tech22@outlook.com>
2026-04-29 22:17:50 -0300

    delete format route.ts

--- apps/web/src/app/api/auth/[...all]/route.ts
+++ apps/web/src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/lib/auth/auth-server";
 
-export const { GET, POST } = toNextJsHandler(auth.handler);
+export const { GET, POST } = toNextJsHandler(auth.handler);
